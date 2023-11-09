569c89fe41c80f5bff8644b330dcd27a8b0d794b
Michael Wu <wurunzhi@hotmail.com>
2023-11-09 22:55:53 +0800

    made layers draw in proper order
    and added todo list

--- Main scenes/world.gd
+++ Main scenes/world.gd
@@ -14,3 +14,19 @@ func _process(delta):
 			layerTemp.new(cursor.gridPos.y)
 			layerTemp.position.x += cursor.gridPos.y * Game.boxDepth.y
 			add_child(layerTemp)
+	else:
+		sort()
+	if Input.is_action_just_pressed("ui_up") \
+	or Input.is_action_just_pressed("ui_down") \
+	or Input.is_action_just_pressed("ui_right") \
+	or Input.is_action_just_pressed("ui_left"):
+		sort()
+
+func sort():
+	var nodesDrawn = 0s
+	for i in range(-15, 17):
+		var tempNode = get_node("Layer" + str(i))
+		if tempNode:
+			move_child(tempNode, nodesDrawn)
+			nodesDrawn += 1
+	move_child(get_node("Cursor"), -1)
